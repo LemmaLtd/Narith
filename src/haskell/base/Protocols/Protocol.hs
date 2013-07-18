3a23a532325c8f9933452dc7d04d7796eecf925f
Saad Talaat <saad.talaat@synapse-labs.com>
2013-07-18 03:11:38 +0200

    Protocol.hs: Typeclass for polymorphic protocol functions
    Ethernet protocol abstract interface

---
+++ src/haskell/base/Protocols/Protocol.hs
@@ -0,0 +1,30 @@
+-- [Narith]
+-- File: Protocol.hs
+-- Author: Saad Talaat
+-- Date: 18th of July 2013
+-- Brief: Protocols type class including 
+--        all protocols constructors if defined.
+
+module Narith.Protocol (Prot(..),Protocol(..)) where
+
+-- define unified datatype holding all types of
+-- network protocols with each value holding a
+-- subtype execlusive to each protocol impleme-
+-- tation and holding its data.
+-- Why this design?
+-- High abstraction and locality therefore more
+-- reliability. Protocols growth will only lead
+-- to growth of Prot datatype with each typeclass
+-- instance execlusive to its module.
+-- Prot (a): where a is an exclusive datatype
+-- to each protocol holding its data
+
+data Prot a = Ethernet a | Bytes a
+
+-- Typeclass of protocols holding function to
+-- protocol structures constructors and data
+-- retrival functions.
+class Protocol a where
+	initProtocol:: a -> Prot b
+	rawProtocol:: a -> Prot b
+	formatProtocol:: a -> Prot b
