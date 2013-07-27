7b503d323b84822224feff3fe13182c770d4a40e
Saad Talaat <saad.talaat@synapse-labs.com>
2013-07-27 09:10:03 +0200

    Udp.py: Bug fixes in Udp

--- src/python/Narith/base/Protocols/Udp.py
+++ src/python/Narith/base/Protocols/Udp.py
@@ -8,42 +8,48 @@ brief:  Structure to hold UDP info
 
 ''' so far all reading from bytes,
     shall do classmethods soon '''
-
+import random
 class Udp(object):
 
 	# Raw
-	__udp = {'src' : None}
-
+	_udp = dict()
+	_pop = []
 	def __init__(self,b):
 		
-		self.__udp['src'] = int(b[:2].encode('hex'),16)
-		self.__udp['dst'] = int(b[2:4].encode('hex'),16)
-		self.__udp['len'] = int(b[4:6].encode('hex'),16)
-		self.__udp['checksum'] = int(b[6:8].encode('hex'),16)
-
-
+		self._udp['src'] = int(b[:2].encode('hex'),16)
+		self._udp['dst'] = int(b[2:4].encode('hex'),16)
+		self._udp['len'] = int(b[4:6].encode('hex'),16)
+		self._udp['checksum'] = int(b[6:8].encode('hex'),16)
+		self._pop = self._udp['src']
+	def getPop(self):
+		return self._pop
+	def getDst(self):
+		return self._udp['dst']
 	###################
 	# Properties
 
 	@property
+	def pop(self):
+		return self._pop
+	@property
 	def src(self):
-		return self.__udp['src']
+		return self._udp['src']
 	@src.setter
 	def src(self,val):
 		if (type(val) != int) and ( val > 0xffff) and (val < 0):
 			raise ValueError,"Malformed Value"
-		self.__udp['src'] = val
+		self._udp['src'] = val
 
 	@property
 	def dst(self):
-		return self.__udp['dst']
+		return self._udp['dst']
 
 	@dst.setter
 	def dst(self,val):
 		if (type(val) != int) and ( val > 0xffff) and (val < 0):
 			raise ValueError,"Malformed Value"
-		self.__udp['dst'] = val
+		self._udp['dst'] = val
 
 	@property
 	def len(self):
-		return self.__udp['len']
+		return self._udp['len']
