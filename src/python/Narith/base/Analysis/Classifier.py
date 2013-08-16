92e665204fc8b0d8f72b55ca5e7af38c19ba0c19
Saad Talaat <saad.talaat@synapse-labs.com>
2013-08-16 14:55:45 +0200

    Analysis[M]: initiated Analysis base module
    Classifier.py: packet classifier (classifies to list of protocols)

---
+++ src/python/Narith/base/Analysis/Classifier.py
@@ -0,0 +1,32 @@
+'''
+[Narith]
+File: 	Classifier.py
+Author: Saad Talaat
+Date:	16th August 2013
+brief:	Classifies raw packet wireframes into Protocols and packets
+'''
+from Narith.base.Pcap.Pcap import Pcap
+from Narith.base.Protocols import Eth#, Arp, IP, Dns, Tcp, Udp, Ftp
+from Narith.base.Packet.Packet import Packet
+class Classifier(object):
+
+	def __init__(self, packets, records=None):
+		if type(packets) != list:
+			raise TypeError, "Unexpected type"
+		self.__packets = []
+		self.__rawPackets = packets
+		self.__records = records
+
+	def classify(self):
+		for p in self.__rawPackets:
+			packet = Packet()
+			parent = Eth.Eth(p[:14])
+			p = p[14:]
+			while parent != None:
+				packet.attach(parent)
+				prot = parent.nextProtocol
+				if prot == None:
+					break
+				parent = prot(p)
+				p = p[parent.length:]
+			self.__packets.append(packet)
