b63966b590184d589ee5d906f70fcf06841f07a5
Saad Talaat <saad.talaat@synapse-labs.com>
2013-09-10 07:56:46 +0200

    IOManager.py: Removed Class variables

--- src/python/Narith/base/External/IOManager.py
+++ src/python/Narith/base/External/IOManager.py
@@ -6,20 +6,16 @@ Date: 	14th July 2013
 brief: 	extensible abstraction for IO Ops
 '''
 import datetime
-
+import mmap
 
 class IOManager():
-	__file_used__ = None
-	__session_info__ = None
-	__mode__	= None
-	__handle__	= None
 
 	def __init__(self, fname, mode):
 		assert fname != ""
-		self.__file_used__ = fname
-		self.__session_info__ = "%s" % datetime.date.today()
-		self.__mode__ = mode
-		self.__handle__ = file(self.__file_used__, self.__mode__)
+		self.__file_used__ 	= fname
+		self.__session_info__ 	= "%s" % datetime.date.today()
+		self.__mode__ 		= mode
+		self.__handle__ 	= file(self.__file_used__, self.__mode__)
 
 	def read(self, *args):
 		if len(args) > 0:
